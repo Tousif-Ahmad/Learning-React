e170da5e976c53e2757c640cf0a497a410ee71b7
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-10-15 11:55:34 +0500

    jsx

---
+++ Jsx/src/App.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  // let name = "sam"
+
+  // function saymyName(){
+  //   return "my name is sam"
+    
+  // }
+
+  // let age = 22;
+  // let name = "sam";
+  // let outOfStock = true;
+  // let arr = ["sam", "sara", "aina", "ayesha", "sana"];
+  // let alpha = <p>Hello How are you </p>
+
+
+  //  
+
+  return (
+    <div>
+      {/* <h1>Hello jsx {saymyName()}</h1> */}
+      {/* <h1>{age >= 18 ? "You can vote" : "you can't vote"}</h1> */}
+      {/* <h1 style={{
+        color:"red",
+        fontSize : "50px",
+        backgroundColor:"teal",
+
+      }}>Hello Style</h1> */}
+
+    {/* {age >= 18 ? <p>You can drive</p> : <p>you can't drive</p>} */}
+
+    {/* {outOfStock && <p>Out of stock</p>} */}
+
+    {/* {arr.map((item, index)=>{
+      return <h1 key={item}>{item}</h1>;
+    })} */}
+
+    {/* {alpha} */}
+
+    {/* {React.createElement("h1", null, "How are you")} */}
+
+    {/* <div>
+      <p>Hello</p>
+    </div> */}
+
+      {/* {React.createElement("div", null, React.createElement("p", null, "Hello"))} */}
+
+      {/* <h1>My name is {name}</h1> */}
+
+      {/* {React.createElement("h1", null, `my name is ${name}`)} */}
+
+
+    </div>
+  )
+}
+
+export default App
